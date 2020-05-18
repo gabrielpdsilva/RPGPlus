b721f1cddcad5c5b55c3373636a8c434a7b58b10
Gabrielpdsilva <gabriel.pimentel.sotc@gmail.com>
2020-05-18 12:31:23 -0300

    first steps using firestore

--- rpgplus/App.js
+++ rpgplus/App.js
@@ -10,10 +10,45 @@ import {NavigationContainer} from '@react-navigation/native';
 
 import {MyDrawer, MyStack} from './Navigations';
 
+//install firebase:
+//expo install firebase
+//https://docs.expo.io/guides/using-firebase/
+
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC9b3MyUCSlwU1X9IQNLnmYhT1BUQi6HBk",
+  authDomain: "rpgplus-1df5e.firebaseapp.com",
+  databaseURL: "https://rpgplus-1df5e.firebaseio.com",
+  projectId: "rpgplus-1df5e",
+  storageBucket: "rpgplus-1df5e.appspot.com",
+  messagingSenderId: "517305234060",
+  appId: "1:517305234060:web:a2a2f1c160eff9bfa1c1a9"
+};
+// Initialize Firebase
+
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 let isLoggedIn = true;
 
 const App = () => {
+
+  /*
+  FIRST STEPS WITH FIRESTORE
+  const dbh = firebase.firestore();
+
+  dbh.collection("characters").doc("mario").set({
+    employment: "plumber",
+    outfitColor: "red",
+    specialAttack: "fireball"
+  })
+  */
 
   return(
 
