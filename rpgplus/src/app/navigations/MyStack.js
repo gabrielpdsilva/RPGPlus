7221427fdf15fd62960b60a5ea96de2be9e4d5de
Gabrielpdsilva <gabriel.pimentel.sotc@gmail.com>
2020-06-25 13:54:34 -0300

    first tests with camera permission

--- rpgplus/src/app/navigations/MyStack.js
+++ rpgplus/src/app/navigations/MyStack.js
@@ -7,34 +7,42 @@ import RegisterScreen from '../screens/RegisterScreen';
 import ForgotPasswordScreen from '../screens/ForgotPasswordScreen';
 import MyDrawer from './MyDrawer';
 
+import Test from '../screens/Test';
+
 const Stack = createStackNavigator();
 
 const MyStack = () => {
     return (
         <Stack.Navigator>
             <Stack.Screen
-            name="Login"
-            component={LoginScreen}
-            options={{headerShown: false}} //the default app bar won't show
+                name="Login"
+                component={LoginScreen}
+                options={{headerShown: false}} //the default app bar won't show
             />
     
             <Stack.Screen
-            name="Register"
-            component={RegisterScreen}
-            options={{headerShown: false}}
+                name="Register"
+                component={RegisterScreen}
+                options={{headerShown: false}}
             />
     
             <Stack.Screen
-            name="Forgot Password"
-            component={ForgotPasswordScreen}
-            options={{headerShown: false}}
+                name="Forgot Password"
+                component={ForgotPasswordScreen}
+                options={{headerShown: false}}
             />
     
             <Stack.Screen
-            name="RPG+"
-            component={MyDrawer}
-            options={{headerShown: false}}
-            />  
+                name="RPG+"
+                component={MyDrawer}
+                options={{headerShown: false}}
+            />
+
+            <Stack.Screen
+                name="Test"
+                component={Test}
+                options={{headerShown: false}}
+            /> 
         </Stack.Navigator>
     );
 }
