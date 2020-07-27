a6093e5714d1e6e3d92482332cc2a766c75de833
Gabrielpdsilva <gabriel.pimentel.sotc@gmail.com>
2020-07-27 16:57:03 -0300

    keeping user logged in after exit the app

--- rpgplus/App.js
+++ rpgplus/App.js
@@ -2,11 +2,11 @@
  * 
  * author: G.P.
  * RPG+ (RPGPlus) is a project made for tests and studies using React Native technology
- * version: 1.18.67
+ * version: 1.19.67
  * 
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
 
@@ -14,9 +14,20 @@ import MyStack from './src/app/navigations/MyStack';
 import MyDrawer from './src/app/navigations/MyDrawer';
 import firebase from './src/app/controller/FirebaseConfig';
 
-const App = () => {
+//useful site about keep user logged in:
+//https://rnfirebase.io/auth/usage
 
-    const user = firebase.auth().currentUser;
+const App = () => {
+    //initial state while firebase is connecting
+    const [initializing, setInitializing] = useState(true);
+    const [user, setUser] = useState();
+
+    firebase.auth().onAuthStateChanged((user) => {
+        setUser(user);
+        if (initializing) setInitializing(false);
+    });
+
+    if (initializing) return null;
 
     return(
 
